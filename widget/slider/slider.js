2b628115dce98f300b9615d7c7b16dab897fc1b5
sophia <sophiaytyt@gmail.com>
2015-03-31 23:09:40 +0800

    use swipe in mobile slider

---
+++ widget/slider/slider.js
@@ -0,0 +1,8 @@
+var Swiper = require('swiper');
+
+var app = module.exports = function(opt) {
+	var swiper = new Swiper(opt.container, {
+        pagination: opt.pagination,
+        paginationClickable: true
+    });
+};
