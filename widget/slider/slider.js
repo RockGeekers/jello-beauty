61afee0eb63394be41897e4b6fddac2399ac9742
yangting <yangting@pinganfang.com>
2015-04-01 09:36:53 +0800

    add files

---
+++ widget/slider/slider.js
@@ -0,0 +1,10 @@
+var $ = require('zepto'),
+	gmu = require('gmu');
+var app = module.exports = function(nav) {
+	// $(function(){
+	// 	$(nav).slider({
+	//     	loop : true,
+	//     	arrow :false
+	//     });
+	// });
+};
